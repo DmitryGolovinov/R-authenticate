8edb3de5b49253aaa24fbfb230fefeba460879df
Coverkincel <coverkincel@gmail.com>
2019-04-29 17:39:22 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class authorization extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      password: Math.floor(Math.random()*20),
+      ifAuth: false,
+      incorrect: false
+    };
+    this.checkPassword = this.checkPassword.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  checkPassword(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password == this.state.password;
+    if (auth) {
+      this.setState({
+        ifAuth: true
+      })
+    } else {
+      this.setState({
+        incorrect: true
+      })
+    }
+    
+  }
+
+  reset() {
+    this.setState({
+      ifAuth: false,
+      incorrect: false,
+      password: Math.floor(Math.random()*20)
+    })
+  }
+
+  render() {
+    const login = (
+      <div className='row' id='row'>
+        <div className='col-lg-4 col-lg-offset-4'>
+        <form className='input-group' action='#' onSubmit={this.checkPassword} >
+        <input type='password' className="form-control" placeolder='Password' autofocus="true"/>
+        <input type='submit' className="btn btn-primary" />
+        </form>
+        </div> 
+      </div> 
+    ); 
+    const Secret = (
+      <h1>You know the password! congratulations!</h1>
+    )
+    return (
+      <div id='authorization'>
+        {this.state.ifAuth ? <div>
+        {Secret}
+        <button onClick={this.reset} className='btn btn-success'>Get back!</button>
+        </div> : <div>
+          {login}
+          {this.state.incorrect ? <p style={{color:'red'}}>Incorrect password. Try again.</p> : <p></p>}
+          </div>}
+        <p style={{position: 'absolute', bottom: '0'}}>Password: {this.state.password}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default authorization;
